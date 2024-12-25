5a10e20d4b0bfdd9ffcd06a279ef13ef50336c05
Irina Khameeva <irina.khameeva@gmail.com>
2024-12-25 11:57:25 -0500

    Custom Hook was removed, Mimicing asynchronous data fetching, Conditional Loading Indicator was created

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,33 @@ import AddTodoForm from './AddTodoForm'
 import InputWithLabel from './InputWithLabel'
 
 
-function useSemiPersistentState() {
-  const [todoList, setTodoList] = useState(JSON.parse(localStorage.getItem("savedTodoList")));
-
-  useEffect(() => {
-    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
-    console.log("Todo list saved to localStorage:", todoList);
-  }, [todoList])
-
-  return [todoList, setTodoList];
-}
 
 function App() {
 
-  const [todoList, setTodoList] = useSemiPersistentState();
+  const [todoList, setTodoList] = useState(JSON.parse(localStorage.getItem("todoList")) || []);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    console.log("creating prom")
+    new Promise((resolve) => 
+      setTimeout(()=>{
+       resolve({
+          todoList: JSON.parse(localStorage.getItem("todoList")) || [],
+       });
+      }, 5000)
+    ).then((result)=>{
+      console.log("result:" + result);
+      setTodoList(result.todoList);
+      setIsLoading(false);
+    });
+  }, []);
+
+  useEffect(() => {
+    if(!isLoading){
+      localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+    }
+    
+  }, [todoList])
 
   function addTodo (newTodo){
     setTodoList([newTodo, ...todoList]);
@@ -29,13 +42,20 @@ function App() {
   const onRemoveTodo = (id) => {
 		setTodoList(todoList.filter(item => item.id !== id));
   }  
+  console.log("app:")
   return (
     <>
       <h1>Todo List</h1>
       <AddTodoForm onAddTodo={addTodo} />
-      
-      <TodoList todoList={todoList} onRemoveTodo={onRemoveTodo} />
-      
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : todoList.length === 0 ? (
+      <p>You have no tasks yet</p> 
+      ) : (
+        <TodoList todoList={todoList} onRemoveTodo={onRemoveTodo} />
+        
+      )}
+            
     </>
   )
 }
--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
 function TodoListItem({item, onRemoveTodo}) {
+    console.log("TLI added item:" + item)
         return (
-        <li>
+        <li key={item.id}>
             {item.title}
             <button type="button" onClick={() => onRemoveTodo(item.id)}>"Remove"</button>
         </li>
