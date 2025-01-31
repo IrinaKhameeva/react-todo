d0e4deff2aba9cd0470bedf71d37aed9a5e581e0
Irina Khameeva <irina.khameeva@gmail.com>
2025-01-30 21:36:45 -0500

    CSS Styled components

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import style from "./TodoListItem.module.css";
 
 function TodoListItem({item, onRemoveTodo}) {
     console.log("TLI added item:" + item)
         return (
-        <li key={item.id}>
+        <li key={item.id} className={style.ListItem}>
             {item.title}
             <button type="button" onClick={() => onRemoveTodo(item.id)}>"Remove"</button>
         </li>
